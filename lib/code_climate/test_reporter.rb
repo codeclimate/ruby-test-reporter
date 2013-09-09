31432b8944c3ba50b71bf1f1d545949b51cca4c3
Noah Davis <noahd1@yahoo.com>
2013-09-09 01:07:55 -0400

    Support tddium parallelized builds

---
+++ lib/code_climate/test_reporter.rb
@@ -0,0 +1,20 @@
+module CodeClimate
+  module TestReporter
+    VERSION = "0.0.6"
+
+    def self.start
+      if run?
+        require "simplecov"
+        ::SimpleCov.add_filter 'vendor'
+        ::SimpleCov.formatter = Formatter
+        ::SimpleCov.start("test_frameworks")
+      else
+        puts("Not reporting to Code Climate because ENV['CODECLIMATE_REPO_TOKEN'] is not set.")
+      end
+    end
+
+    def self.run?
+      !!ENV["CODECLIMATE_REPO_TOKEN"]
+    end
+  end
+end
