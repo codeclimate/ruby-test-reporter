8c1c77122ea0aead09f26fd32ec88154a9d013fa
Marten Veldthuis <marten@veldthuis.com>
2013-09-11 13:45:49 +0200

    Configure VCR to ignore the submission of coverage results to Code Climate

--- lib/code_climate/test_reporter.rb
+++ lib/code_climate/test_reporter.rb
@@ -8,6 +8,10 @@ module CodeClimate
         ::SimpleCov.add_filter 'vendor'
         ::SimpleCov.formatter = Formatter
         ::SimpleCov.start("test_frameworks")
+        
+        if defined?(VCR)
+          VCR.configure { |c| c.ignore_hosts "codeclimate.com" }
+        end
       else
         puts("Not reporting to Code Climate because ENV['CODECLIMATE_REPO_TOKEN'] is not set.")
       end
