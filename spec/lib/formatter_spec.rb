fd5fc7698e22e7e703f440c31ccb91ca953bc1b8
Noah Davis <noahd1@yahoo.com>
2013-09-20 19:28:09 -0400

    Making integration test less fragile

--- spec/lib/formatter_spec.rb
+++ spec/lib/formatter_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
+require 'fileutils'
 
 module CodeClimate::TestReporter
   describe Formatter do
+    let(:project_path) { "spec/tmp" }
+    let(:project_file) { "fake_project.rb" }
     let(:formatter) { Formatter.new }
     let(:files) {
       [
@@ -9,7 +12,7 @@ module CodeClimate::TestReporter
           :lines            => [mock, mock, mock],
           :covered_lines    => [mock, mock],
           :missed_lines     => [mock],
-          :filename         => "lib/code_climate/test_reporter.rb",
+          :filename         => project_file,
           :coverage         => [0,3,2,nil],
           :covered_percent  => 33.2,
           :covered_strength => 2
@@ -27,49 +30,68 @@ module CodeClimate::TestReporter
       )
     }
 
-    it "sends an http request with all the coverage information" do
-      app = MyRackApp.new do |env|
-        env["PATH_INFO"].should == "/test_reports"
-        puts JSON.parse(env["rack.input"].string).should ==
+    let(:expected_request) {
+      {
+        "repo_token" => "172754c1bf9a3c698f7770b9fb648f1ebb214425120022d0b2ffc65b97dff531",
+        "source_files" =>
+          [
+            {
+              "name" => project_file,
+              "blob_id" => "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+              "coverage" => "[0,3,2,null]",
+              "covered_percent" => 33.2,
+              "covered_strength" => 2.0,
+              "line_counts" => {"total"=>3, "covered"=>2, "missed"=>1}
+            }
+          ],
+        "run_at" => 1379704336,
+        "covered_percent" => 24.3,
+        "covered_strength" => 33.2,
+        "line_counts" => {"total" => 3, "covered" => 2, "missed" => 1 },
+        "partial"=> false,
+        "git" =>
           {
-            "repo_token" => "172754c1bf9a3c698f7770b9fb648f1ebb214425120022d0b2ffc65b97dff531",
-            "source_files" =>
-              [
-                {
-                  "name" => "lib/code_climate/test_reporter.rb",
-                  "blob_id" => "bfa3656d591dab2cf9ca6d3df53aac71974a45f4",
-                  "coverage" => "[0,3,2,null]",
-                  "covered_percent" => 33.2,
-                  "covered_strength" => 2.0,
-                  "line_counts" => {"total"=>3, "covered"=>2, "missed"=>1}
-                }
-              ],
-            "run_at" => 1379704336,
-            "covered_percent" => 24.3,
-            "covered_strength" => 33.2,
-            "line_counts" => {"total" => 3, "covered" => 2, "missed" => 1 },
-            "partial"=> false,
-            "git" =>
-              {
-                "head" => "0ebc98c64d39fe40d362dcc555b76230a07b8732",
-                "committed_at" => 1379702812,
-                "branch" => "master"
-              },
-            "environment" =>
-              {
-                "test_framework" => "rspec",
-                "pwd" => "/Users/noah/p/ruby-test-reporter",
-                "rails_root" => nil,
-                "simplecov_root" => "/Users/noah/p/ruby-test-reporter",
-                "gem_version" => "0.0.11"
-              },
-            "ci_service" => {}
-          }
-      end
+            "head" => @commit_sha,
+            "committed_at" => @committed_at.to_i,
+            "branch" => "master"
+          },
+        "environment" =>
+          {
+            "test_framework" => "rspec",
+            "pwd" => Dir.pwd,
+            "rails_root" => nil,
+            "simplecov_root" => Dir.pwd,
+            "gem_version" => "0.0.11"
+          },
+        "ci_service" => {}
+      }
+    }
 
+    before do
+      @old_pwd = Dir.pwd
+      FileUtils.mkdir_p(project_path)
+      FileUtils.cd(project_path)
+      FileUtils.touch(project_file)
+      SimpleCov.root(Dir.pwd)
+      system("git init")
+      system("git add #{project_file}")
+      system("git commit -m 'initial commit'")
+      @commit_sha = `git log -1 --pretty=format:'%H'`
+      @committed_at = `git log -1 --pretty=format:'%ct'`
+    end
+
+    after do
+      FileUtils.cd(@old_pwd)
+      FileUtils.rm_rf(project_path)
+    end
+
+    it "sends an http request with all the coverage information" do
+      app = FakeCodeClimateEndpoint.new
       Artifice.activate_with(app) do
         formatter.format(simplecov_result)
       end
+      app.path_info.should == "/test_reports"
+      JSON.parse(app.request_body).should == expected_request
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,27 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'artifice'
+require 'pry'
 
 ENV['CODECLIMATE_REPO_TOKEN'] = "172754c1bf9a3c698f7770b9fb648f1ebb214425120022d0b2ffc65b97dff531"
 ENV['CODECLIMATE_API_HOST']   = "http://cc.dev"
 
-class MyRackApp
-  def initialize(&block)
-    @expection = block
-  end
+class FakeCodeClimateEndpoint
   def call(env)
-    @expection.call(env)
+    @env = env
     [
       200,
       {"Content-Type" => 'text/plain'},
       ["Received"]
     ]
+  end
+
+  def path_info
+    @env["PATH_INFO"]
+  end
+
+  def request_body
+    @env["rack.input"].string
   end
 end
 
