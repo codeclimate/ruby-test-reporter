5fb963c1b6c20639fb32066efcbb8eb828533a74
Noah Davis <noahd1@yahoo.com>
2013-09-21 23:37:10 -0400

    Move requirement of version earlier

--- lib/codeclimate-test-reporter.rb
+++ lib/codeclimate-test-reporter.rb
@@ -1,4 +1,5 @@
 require "code_climate/test_reporter"
+require "code_climate/test_reporter/version"
 require "code_climate/test_reporter/client"
 require "code_climate/test_reporter/formatter"
-require "code_climate/test_reporter/version"
+
