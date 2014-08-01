c2f9828617db2f7764fa1ce516c130018fba49a2
David Calavera <david.calavera@gmail.com>
2014-08-01 12:01:26 -0700

    Check in the formatter if it's allowed to be run.

--- lib/code_climate/test_reporter.rb
+++ lib/code_climate/test_reporter.rb
@@ -15,23 +15,27 @@ module CodeClimate
     end
 
     def self.environment_variable_set?
-      environment_variable_set = !!ENV["CODECLIMATE_REPO_TOKEN"]
-      unless environment_variable_set
+      return @environment_variable_set if defined?(@environment_variable_set)
+
+      @environment_variable_set = !!ENV["CODECLIMATE_REPO_TOKEN"]
+      unless @environment_variable_set
         logger.info("Not reporting to Code Climate because ENV['CODECLIMATE_REPO_TOKEN'] is not set.")
       end
 
-      environment_variable_set
+      @environment_variable_set
     end
 
     def self.run_on_current_branch?
-      return true if configured_branch.nil?
+      return @run_on_current_branch if defined?(@run_on_current_branch)
 
-      run_on_current_branch = !!(current_branch =~ /#{configured_branch}/i)
-      unless run_on_current_branch
+      @run_on_current_branch = true if configured_branch.nil?
+      @run_on_current_branch ||= !!(current_branch =~ /#{configured_branch}/i)
+
+      unless @run_on_current_branch
         logger.info("Not reporting to Code Climate because #{configured_branch} is set as the reporting branch.")
       end
 
-      run_on_current_branch
+      @run_on_current_branch
     end
 
     def self.configured_branch
