98f729f8fdb4585334b0123665f9b3df623470af
Michael Grosser <michael@grosser.it>
2015-03-21 00:24:58 +0100

    unify files before submitting

--- lib/code_climate/test_reporter/client.rb
+++ lib/code_climate/test_reporter/client.rb
@@ -16,6 +16,8 @@ module CodeClimate
       end
 
       def batch_post_results(files)
+        return if files.size == 0
+        file, content = unify_simplecov(files)
         uri = URI.parse("#{host}/test_reports/batch")
         http = http_client(uri)
 
@@ -25,13 +27,11 @@ module CodeClimate
         post_body << "Content-Disposition: form-data; name=\"repo_token\"\r\n"
         post_body << "\r\n"
         post_body << ENV["CODECLIMATE_REPO_TOKEN"]
-        files.each_with_index do |file, index|
-          post_body << "\r\n--#{boundary}\r\n"
-          post_body << "Content-Disposition: form-data; name=\"coverage_reports[#{index}]\"; filename=\"#{File.basename(file)}\"\r\n"
-          post_body << "Content-Type: application/json\r\n"
-          post_body << "\r\n"
-          post_body << File.read(file)
-        end
+        post_body << "\r\n--#{boundary}\r\n"
+        post_body << "Content-Disposition: form-data; name=\"coverage_reports[0]\"; filename=\"#{File.basename(file)}\"\r\n"
+        post_body << "Content-Type: application/json\r\n"
+        post_body << "\r\n"
+        post_body << content
         post_body << "\r\n--#{boundary}--\r\n"
         request = Net::HTTP::Post.new(uri.request_uri)
         request["User-Agent"] = USER_AGENT
@@ -71,6 +71,60 @@ module CodeClimate
       end
 
     private
+
+      # turn 10 files into 1 file with the sum of all coverages
+      def unify_simplecov(coverage_files)
+        return [coverage_files.first, File.read(coverage_files.first)] if coverage_files.size == 1
+
+        combined = coverage_files.shift
+        puts "Unifying #{coverage_files.size + 1} files into #{combined}"
+
+        report = JSON.load(File.read(combined))
+        coverage_files.each do |file|
+          merge_source_files(report, JSON.load(File.read(file)).fetch("source_files"))
+        end
+        recalculate_counters(report)
+        [combined, report.to_json]
+      end
+
+      def recalculate_counters(report)
+        source_files = report.fetch("source_files").map { |s| s["line_counts"] }
+        report["line_counts"].keys.each do |k|
+          report["line_counts"][k] = source_files.map { |s| s[k] }.inject(:+)
+        end
+      end
+
+      def merge_source_files(report, source_files)
+        all = report.fetch("source_files")
+        source_files.each do |new_file|
+          old_file = all.detect { |source_file| source_file["name"] == new_file["name"] }
+
+          if old_file
+            # merge source files
+            coverage = merge_coverage(
+              JSON.load(new_file.fetch("coverage")),
+              JSON.load(old_file.fetch("coverage"))
+            )
+            old_file["coverage"] = JSON.dump(coverage)
+
+            total = coverage.size
+            missed, covered = coverage.compact.partition { |l| l == 0 }.map(&:size)
+            old_file["covered_percent"] = covered * 100.0 / (covered + missed)
+            old_file["line_counts"] = {"total" => total, "covered" => covered, "missed" => missed}
+          else
+            # just use the new value
+            all << new_file
+          end
+        end
+      end
+
+      # [nil,1,0] + [nil,nil,2] -> [nil,1,2]
+      def merge_coverage(a,b)
+        b.map! do |b_count|
+          a_count = a.shift
+          (!b_count && !a_count) ? nil : b_count.to_i + a_count.to_i
+        end
+      end
 
       def http_client(uri)
         Net::HTTP.new(uri.host, uri.port).tap do |http|
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -24,6 +24,16 @@ module CodeClimate::TestReporter
     describe "#batch_post_results" do
       let(:uuid) { "my-uuid" }
       let(:token) { ENV["CODECLIMATE_REPO_TOKEN"] }
+      let(:request_start) { "--#{uuid}\r\nContent-Disposition: form-data; name=\"repo_token\"\r\n\r\n#{token}\r\n--#{uuid}\r\nContent-Disposition: form-data; name=\"coverage_reports[0]\"; filename=\"a\"\r\nContent-Type: application/json\r\n\r\n" }
+      let(:request_end) { "\r\n--#{uuid}--\r\n" }
+
+      def remove_boilerplate(response)
+        [request_start, request_end].each do |part|
+          expect(response).to include part
+          response = response.sub(part, "")
+        end
+        response
+      end
 
       before { expect(SecureRandom).to receive(:uuid).and_return uuid }
       around { |test| Dir.mktmpdir { |dir| Dir.chdir(dir, &test) } }
@@ -33,7 +43,37 @@ module CodeClimate::TestReporter
         requests = capture_requests(stub_request(:post, "http://cc.dev/test_reports/batch"))
         Client.new.batch_post_results(["a"])
 
-        expect(requests.first.body).to eq "--#{uuid}\r\nContent-Disposition: form-data; name=\"repo_token\"\r\n\r\n#{token}\r\n--#{uuid}\r\nContent-Disposition: form-data; name=\"coverage_reports[0]\"; filename=\"a\"\r\nContent-Type: application/json\r\n\r\nSomething\r\n--#{uuid}--\r\n"
+        expect(remove_boilerplate(requests.first.body)).to eq "Something"
+      end
+
+      it "merges multiple files" do
+        a = {
+          "source_files" => [
+            {"name" => "a", "coverage" => "[null,0,0,null]", "foo" => "bar", "line_counts" => {"total" => 4, "covered" => 0, "missed" => 2}}
+          ],
+          "line_counts" => {"total" => 4, "covered" => 0, "missed" => 2}
+        }
+        File.write("a", a.to_json)
+
+        b = {
+          "source_files" => [
+            {"name" => "a", "coverage" => "[null,3,0,null]", "line_counts" => {"total" => 4, "covered" => 1, "missed" => 1}},
+            {"name" => "b", "coverage" => "[null,1,1,null]", "line_counts" => {"total" => 4, "covered" => 2, "missed" => 0}, "covered_percent" => 100.0}
+          ],
+          "line_counts" => {"total" => 8, "covered" => 3, "missed" => 1}
+        }
+        File.write("b", b.to_json)
+
+        requests = capture_requests(stub_request(:post, "http://cc.dev/test_reports/batch"))
+        Client.new.batch_post_results(["a", "b"])
+
+        expect(JSON.load(remove_boilerplate(requests.first.body))).to eq(
+          "source_files"=>[
+            {"name"=>"a", "coverage"=>"[null,3,0,null]", "foo"=>"bar", "line_counts"=>{"total"=>4, "covered"=>1, "missed"=>1}, "covered_percent"=>50.0},
+            {"name"=>"b", "coverage"=>"[null,1,1,null]", "line_counts"=>{"total"=>4, "covered"=>2, "missed"=>0}, "covered_percent"=>100.0}
+          ],
+          "line_counts"=>{"total"=>8, "covered"=>3, "missed"=>1}
+        )
       end
     end
   end
