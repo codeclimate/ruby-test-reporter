b05167ebd32353fa3b0f57794c83a23546699248
Mark Olson <mark.olson@livingsocial.com>
2015-04-30 18:07:57 -0400

    Honor CODECLIMATE_API_HOST in VCR/Webmock exception help messages

--- lib/code_climate/test_reporter/exception_message.rb
+++ lib/code_climate/test_reporter/exception_message.rb
@@ -6,9 +6,9 @@ module CodeClimate
         "WebMock"
       end
 
-      def instructions
+      def instructions(api_host)
       <<-STR
-  WebMock.disable_net_connect!(:allow => "codeclimate.com")
+  WebMock.disable_net_connect!(:allow => '#{api_host}')
 STR
       end
     end
@@ -18,11 +18,11 @@ STR
         "VCR"
       end
 
-      def instructions
+      def instructions(api_host)
       <<-STR
   VCR.configure do |config|
     # your existing configuration
-    config.ignore_hosts 'codeclimate.com'
+    config.ignore_hosts '#{api_host}'
   end
 STR
       end
@@ -40,14 +40,15 @@ STR
       end
 
       def message
+        api_host = URI(ENV["CODECLIMATE_API_HOST"] || "https://codeclimate.com").host
         parts = []
         parts << "Code Climate encountered an exception: #{exception_class}"
         if http_stubbing_exception
           message = http_stubbing_exception.new
           parts << "======"
-          parts << "Hey! Looks like you are using #{message.library_name}, which will prevent the codeclimate-test-reporter from reporting results to codeclimate.com.
+          parts << "Hey! Looks like you are using #{message.library_name}, which will prevent the codeclimate-test-reporter from reporting results to #{api_host}.
 Add the following to your spec or test helper to ensure codeclimate-test-reporter can post coverage results:"
-          parts << "\n" + message.instructions + "\n"
+          parts << "\n" + message.instructions(api_host) + "\n"
           parts << "======"
           parts << "If this doesn't work, please consult https://codeclimate.com/docs#test-coverage-troubleshooting"
           parts << "======"
