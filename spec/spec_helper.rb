6490ca3ada20775b709875fb5fadd8918f109da3
patrick brisbin <pbrisbin@gmail.com>
2016-09-12 20:52:50 -0400

    Silence noisy specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,20 @@ module TestHelper
     stub.to_return { |r| requests << r; {body: "hello"} }
     requests
   end
+
+  def capture_io
+    stdout = $stdout
+    stderr = $stderr
+    $stdout = StringIO.new
+    $stderr = StringIO.new
+
+    yield if block_given?
+
+    [$stdout, $stderr]
+  ensure
+    $stdout = stdout
+    $stderr = stderr
+  end
 end
 
 RSpec.configure do |c|
